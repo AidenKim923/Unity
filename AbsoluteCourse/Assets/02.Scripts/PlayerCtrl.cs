5b6ee2ed627546610abfa1d562df622c229bda3d
Aiden <devaiden923@gmail.com>
2024-03-29 08:01:52 +0900

    absolute initial setting

---
+++ AbsoluteCourse/Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private Transform tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        tr.position += Vector3.forward * 1;
+    }
+}
