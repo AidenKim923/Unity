fe938a05bd25ac5f488b8cffceca1a78be80fccf
Aiden <devaiden923@gmail.com>
2024-04-07 10:49:55 +0900

    7장 유니티UI

---
+++ AbsoluteCourse/Assets/02.Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Button startButton;
+    public Button optionButton;
+    public Button shopButton;
+
+    private UnityAction action;
+
+    private void Start()
+    {
+        // UnityAction을 이용한 이벤트 처리
+        action = () => OnButtonClick(startButton.name);
+        startButton.onClick.AddListener(action);
+
+        // 무명 메소드를 활용한 이벤트 연결 방식
+        optionButton.onClick.AddListener(delegate { OnButtonClick(optionButton.name)});
+
+        // 람다식을 활용한 이벤트 연결 방식
+        shopButton.onClick.AddListener(() => OnButtonClick(shopButton.name));
+
+
+    }
+
+
+    public void OnButtonClick(string msg)
+    {
+        Debug.Log($"Click Button : {msg}");
+    }
+}
